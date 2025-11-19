d38ce3e0218f181dc5f3419d9d2a5aec15fc2a89
Sebastian <smartinez.4853@unimar.edu.ve>
2025-11-18 23:48:19 -0400

    Empezando footer

---
+++ Footer.html.php
@@ -0,0 +1,83 @@
+<footer>
+    <div id="Footer_Responsive" class="container-fluid bg-dark mt-5">
+        <div class="row text-align-center p-5 d-lg-none">
+            <div class="accordion" id="accordionPanelsStayOpenExample">
+            <div class="accordion-item mb-2">
+                <h2 class="accordion-header">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
+                    Accordion Item #1
+                </button>
+                </h2>
+                <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse">
+                <div class="accordion-body">
+                    <strong>This is the first item’s accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It’s also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                </div>
+                </div>
+            </div>
+            <div class="accordion-item mb-2">
+                <h2 class="accordion-header">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
+                    Accordion Item #2
+                </button>
+                </h2>
+                <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse">
+                <div class="accordion-body">
+                    <strong>This is the second item’s accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It’s also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                </div>
+                </div>
+            </div>
+            <div class="accordion-item mb-2">
+                <h2 class="accordion-header">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
+                    Accordion Item #3
+                </button>
+                </h2>
+                <div id="panelsStayOpen-collapseThree" class="accordion-collapse collapse">
+                <div class="accordion-body">
+                    <strong>This is the third item’s accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It’s also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
+    </div>
+    <div>
+        <div class="container-fluid">
+            <div class="row bg-dark text-white text-center p-5 d-none d-lg-flex">
+                <div class="col-lg-4">
+                    <p class="h3">Uni-Gigs</p>
+                </div>
+                <div class="col-lg-4">
+                    <p class="h5">AUXILIO</p>
+                    <div class="mb-2">
+                        <a class="text-secondary text-decoration-none" href="#">AUXILIO</a>
+                    </div>
+                    <div>
+                        <a class="text-secondary text-decoration-none" href="#">AUXILIO</a>
+                    </div>
+                    <div class="mb-2">
+                        <a class="text-secondary text-decoration-none" href="#">AUXILIO</a>
+                    </div>
+                    <div class="mb-2">
+                        <a class="text-secondary text-decoration-none" href="#">AUXILIO</a>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <p class="h5">AUXILIO</p>
+                    <div class="mb-2">
+                        <a class="text-secondary text-decoration-none" href="#">AUXILIO</a>
+                    </div>
+                    <div class="mb-2">
+                        <a class="text-secondary text-decoration-none" href="#">AUXILIO</a>
+                    </div>
+                    <div class="mb-2">
+                        <a class="text-secondary text-decoration-none" href="#">AUXILIO</a>
+                    </div>
+                    <div class="mb-2">
+                        <a class="text-secondary text-decoration-none" href="#">AUXILIO</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
---
+++ Index.html.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+    /*-- Esto solamente es para rellenar, obviamente no se vería así (●'◡'●) --*/
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12 col-md-12 col-lg-12 bg-primary text-white p-3 mt-5 mx-auto">
+                Responsive Column
+            </div>
+            <div class="col-sm-12 col-md-12 col-lg-12 bg-secondary text-white p-3 mt-5 mx-auto">
+                Responsive Column
+            </div>
+            <div class="col-sm-12 col-md-12 col-lg-12 bg-success text-white p-3 mt-5 mx-auto">
+                Responsive Column
+            </div>
+        </div>
+    </div>
+    
+<?php include 'Footer.html.php'; ?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html> 
