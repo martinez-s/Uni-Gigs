42121ca1f1cff71f7ec80026574dbe6603236efa
Isabella Garrido <igarrido.7420@unimar.edu.ve>
2025-11-21 22:15:47 -0400

    footer de sebas

--- Footer.html.php
+++ Footer.html.php
@@ -1,6 +1,6 @@
 <footer>
     <div id="Footer_Responsive" class="container-fluid bg-dark">
-        <div class="row text-align-center p-5 d-lg-none">
+        <div class="row text-align-center p-5 d-md-none d-lg-none">
             <div class="accordion" id="accordionPanelsStayOpenExample" data-bs-theme="dark">
             <div class="accordion-item mb-2 border-0 border-bottom">
                 <h2 class="accordion-header">
@@ -83,8 +83,8 @@
     </div>
     <div>
         <div id="Footer_Large" class="container-fluid">
-            <div class="row bg-dark text-white text-center p-5 d-none d-lg-flex">
-                <div class="col-lg-3">
+            <div class="row bg-dark text-white text-center p-5 d-none d-md-flex d-lg-flex">
+                <div class="col-lg-3 col-md-3">
                     <div class="Titulo d-flex justify-content-center align-items-center mb-3">
                         <img src="img/Isotipo_Blanco.png" alt="Logo" width="60" height="48" class="d-inline-block align-text-center me-2">
                         <span class="h3 mb-0">Uni-Gigs</span>                        
@@ -95,7 +95,7 @@
                         <i class="bi bi-twitter-x fs-2 text-white p-3"></i>
                     </div>
                 </div>
-                <div class="col-lg-3">
+                <div class="col-lg-3 col-md-3">
                     <p class="h5">AUXILIO</p>
                     <div class="mb-2">
                         <a class="text-secondary text-decoration-none" href="#">AUXILIO</a>
@@ -110,7 +110,7 @@
                         <a class="text-secondary text-decoration-none" href="#">AUXILIO</a>
                     </div>
                 </div>
-                <div class="col-lg-3">
+                <div class="col-lg-3 col-md-3">
                     <p class="h5">AUXILIO</p>
                     <div class="mb-2">
                         <a class="text-secondary text-decoration-none" href="#">AUXILIO</a>
@@ -125,7 +125,7 @@
                         <a class="text-secondary text-decoration-none" href="#">AUXILIO</a>
                     </div>
                 </div>
-                <div class="col-lg-3">
+                <div class="col-lg-3 col-md-3">
                     <p class="h5">AUXILIO</p>
                     <div class="mb-2">
                         <a class="text-secondary text-decoration-none" href="#">AUXILIO</a>
